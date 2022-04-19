a935d23971b68cb88d0e58d6be3d4789f6d912ae
Aaron Klem <aaronnicolasklem@gmail.com>
2022-04-18 22:31:16 -0400

    Add index html file

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-Oracle 
+//Oracle 
+const express = require('express');
+const port = 3000;
+const app = express();
+const path = require('path');
+const oracleRouter= require(`./routes/oracleRouter`)
+app.use ('/api/v1/oracle',oracleRouter);
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
