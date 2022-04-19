4a91c25cb7917bbc097fbe3a1bd9f33e8ca266ae
Aaron Klem <aaronnicolasklem@gmail.com>
2022-04-18 23:29:30 -0400

    Add routes for world and refs catagorites

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const port = 3000;
 const app = express();
 const path = require('path');
-const oracleRouter= require(`./routes/oracleRouter`)
+///////
+const oracleRouter= require(`./routes/oracleRouter`);
+const worldRouter= require(`./routes/worldRouter`);
+const refsRouter= require(`./routes/refsRouter`);
+
 app.use ('/api/v1/oracle',oracleRouter);
 
 app.get('/', function(req, res) {
